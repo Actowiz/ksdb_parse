5f08a97a447a9b34314777428f068eb43b159074
Rheabactowiz <121953752+Rheabactowiz@users.noreply.github.com>
2024-03-22 12:14:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ksdb_parse",
-    version="0.0.5",
+    version="0.0.6",
     description="",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
