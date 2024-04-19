9e9c880626860897a8e644035798382f2ad554a3
vishala-pc <107846242+vishala-pc@users.noreply.github.com>
2024-04-19 09:20:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ksdb_parse",
-    version="0.0.16",
+    version="0.0.17",
     description="",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
